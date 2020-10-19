48e0108d3d4d3e45485b748352d527700066557b
DenisDoroshko <de.doroshko@mail.ru>
2020-10-19 20:09:01 +0300

    final commit

--- lab_work3/Matrix/Matrix.cs
+++ lab_work3/Matrix/Matrix.cs
@@ -5,116 +5,73 @@ namespace Matrixes
     /// <summary>
     /// Represents a matrix of real numbers
     /// </summary>
+
     public class Matrix
     {
         private double[,] numbers;
         private int numberRows;
         private int numberColumns;
+
         /// <summary>
         /// Two-dimensional array of real numbers
         /// </summary>
+
         public double[,] Numbers
         {
             get { return numbers; }
+            set { numbers = value; }
 
         }
+
         /// <summary>
-        /// Number of lines
+        /// Number of rows
         /// </summary>
+
         public int NumberRows
         {
             get { return numberRows; }
+            set { numberRows = value; }
         }
+
         /// <summary>
         /// Number of columns
         /// </summary>
+
         public int NumberColumns
         {
             get { return numberColumns; }
+            set { numberColumns = value; }
         }
+
         /// <summary>
         /// Creates an instance of the matrix
         /// </summary>
         /// <param name="NumberRows">Number of lines</param>
         /// <param name="NumberColumns">Number of columns</param>
         /// <param name="Numbers">Two-dimensional array of real numbers</param>
+
         public Matrix(int NumberRows, int NumberColumns, double[,] Numbers)
         {
             numberRows = NumberRows;
             numberColumns = NumberColumns;
             numbers = Numbers;
         }
+
         /// <summary>
         /// Creates an instance of the matrix
         /// </summary>
+
         public Matrix()
         {
 
         }
-        /// <summary>
-        /// Вводит матрицу
-        /// </summary>
-        /// <returns>Has the matrix been entered</returns>
-        public bool InputMatrix()
-        {
-            int rows;
-            int columns;
-            bool resultInput = true;
-            Console.WriteLine("Введите количество строк матрицы ");
-            int.TryParse(Console.ReadLine(), out rows);
-            Console.WriteLine("Введите количество столбцов матрицы ");
-            int.TryParse(Console.ReadLine(), out columns);
-            if (rows > 0 && columns > 0)
-            {
-                double[,] tempMassiv = new double[rows, columns];
-                for (int i = 0; i < rows; i++)
-                {
-                    for (int j = 0; j < columns; j++)
-                    {
-                        Console.WriteLine($"Введите элемент {i + 1}-строки {j + 1}-столбца ");
-                        if (!double.TryParse(Console.ReadLine(), out tempMassiv[i, j]))
-                        {
-                            Console.WriteLine("Некоректный ввод");
-                            resultInput = false;
-                        }
-                    }
-                }
-                if (resultInput)
-                {
-                    numbers = tempMassiv;
-                    numberRows = rows;
-                    numberColumns = columns;
-                }
-                else
-                {
-                    resultInput = false;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Некоректный ввод количества строк-столбцов");
-            }
-            return resultInput;
-        }
-        /// <summary>
-        /// Outputs the matrix
-        /// </summary>
-        public void ShowMatrix()
-        {
-            for (int i = 0; i < numberRows; i++)
-            {
-                for (int j = 0; j < numberColumns; j++)
-                {
-                    Console.Write($"{numbers[i, j]} ");
-                }
-                Console.Write("\n");
-            }
-        }
+
         /// <summary>
         /// Calculates the sum of squares of matrix elements larger than the specified number
         /// </summary>
         /// <param name="number"></param>
         /// <returns>Sum of squares</returns>
+
         public double CalculateSumSquares(double number)
         {
             double sum = 0;
@@ -130,12 +87,14 @@ namespace Matrixes
             }
             return sum;
         }
+
         /// <summary>
         /// Calculates the sum of squares of elements located after the specified element
         /// </summary>
         /// <param name="row">Row number of the specified element</param>
         /// <param name="column">Column number of the specified element</param>
         /// <returns>Sum of squares</returns>
+
         public double CalculateSumSquares(int row, int column)
         {
             double sum = 0;
@@ -150,6 +109,7 @@ namespace Matrixes
             }
             return sum;
         }
+
         /// <summary>
         /// Calculates the product of two matrices
         /// </summary>
@@ -157,6 +117,7 @@ namespace Matrixes
         /// <param name="secondMatrix">The second source matrix</param>
         /// <param name="resultMatrix">A new matrix that is the result of the product</param>
         /// <returns>The value is true if the work was completed correctly; otherwise, the value is false</returns>
+
         public static bool ScalarMultiple(Matrix firstMatrix, Matrix secondMatrix, ref Matrix resultMatrix)
         {
             int indexColumn;
@@ -184,34 +145,40 @@ namespace Matrixes
             }
             return resultOperation;
         }
+
         /// <summary>
         /// Operator overloading is the relationship between the two matrices
         /// </summary>
         /// <param name="firstMatrix">The first matrix</param>
         /// <param name="secondMatrix">The second matrix</param>
         /// <returns>True if the matrices are equal; otherwise, false</returns>
-        public static bool operator == (Matrix firstMatrix, Matrix secondMatrix)
+
+        public static bool operator ==(Matrix firstMatrix, Matrix secondMatrix)
         {
             return firstMatrix.Equals(secondMatrix);
         }
+
         /// <summary>
         /// Operator overloading is the relationship between the two matrices
         /// </summary>
         /// <param name="firstMatrix">The first matrix</param>
         /// <param name="secondMatrix">The second matrix</param>
         /// <returns>True if the matrices are not equal; otherwise, false</returns>
+
         public static bool operator !=(Matrix firstMatrix, Matrix secondMatrix)
         {
             return !firstMatrix.Equals(secondMatrix);
         }
+
         /// <summary>
         /// Redefining the Equals method
         /// </summary>
         /// <param name="obj"></param>
         /// <returns>True if the specified object is equal to the current object; otherwise, false</returns>
+
         public override bool Equals(object obj)
         {
-            if(obj == null || GetType()!= obj.GetType())
+            if (obj == null || GetType() != obj.GetType())
             {
                 return false;
             }
@@ -221,12 +188,12 @@ namespace Matrixes
                 if (numberRows != checkMatrix.NumberRows || numberColumns != checkMatrix.NumberColumns)
                 {
                     return false;
-                }    
+                }
                 else
                 {
-                    for(int i=0; i < numberRows; i++)
+                    for (int i = 0; i < numberRows; i++)
                     {
-                        for(int j=0; j <numberColumns; j++)
+                        for (int j = 0; j < numberColumns; j++)
                         {
                             if (numbers[i, j] != checkMatrix.Numbers[i, j])
                                 return false;
@@ -236,13 +203,15 @@ namespace Matrixes
             }
             return true;
         }
+
         /// <summary>
         /// Redefining the GetHashCode method that calculates the hash code of the current object
         /// </summary>
         /// <returns>Hash code of the current object</returns>
+
         public override int GetHashCode()
         {
-            return numberRows * numberColumns /2 ;
+            return numberRows * numberColumns / 2;
         }
     }
 }
--- lab_work3/UserInterface/InteractionMethods.cs
+++ lab_work3/UserInterface/InteractionMethods.cs
@@ -13,26 +13,28 @@ namespace UserInterface
         /// </summary>
         public static void ShowMenu()
         {
-            int choice;
-            bool inputStatus=false;
+            const int exit = 6;
+            bool inputStatus = false;
             Matrix matrixA = new Matrix();
             Matrix matrixB = new Matrix();
             Matrix matrixC = new Matrix();
-            while ((choice = InputChoice()) != 6)
+            int choice = 0;
+            while (choice != exit)
             {
+                choice = InputChoice();
                 Console.Clear();
                 switch (choice)
                 {
                     case 1:
-                        bool inputResult=true;
+                        bool inputResult = true;
                         Console.WriteLine("Ввод матрицы А:");
-                        if (!matrixA.InputMatrix())
+                        if (!InputMatrix(ref matrixA))
                             inputResult = false;
                         Console.WriteLine("Ввод матрицы B:");
-                        if (!matrixB.InputMatrix())
+                        if (!InputMatrix(ref matrixB))
                             inputResult = false;
                         Console.WriteLine("Ввод матрицы C:");
-                        if (!matrixC.InputMatrix())
+                        if (!InputMatrix(ref matrixC))
                             inputResult = false;
                         if (inputResult)
                             inputStatus = true;
@@ -43,15 +45,14 @@ namespace UserInterface
                         if (inputStatus)
                         {
                             Console.WriteLine("Матрица А:");
-                            matrixA.ShowMatrix();
+                            ShowMatrix(matrixA);
                             Console.WriteLine("Матрица B:");
-                            matrixB.ShowMatrix();
+                            ShowMatrix(matrixB);
                             Console.WriteLine("Матрица C:");
-                            matrixC.ShowMatrix();
+                            ShowMatrix(matrixC);
                         }
                         else
                             Console.WriteLine("Нужно ввести матрицы");
-                        
                         break;
                     case 3:
                         if (inputStatus)
@@ -67,7 +68,7 @@ namespace UserInterface
                         {
                             double result = CalculateLocatedSumSquares(matrixA, matrixB, matrixC);
                             if (result != 0)
-                                Console.WriteLine($"Сумма квадратов элементов матриц А и В больших 83.6:{result}");
+                                Console.WriteLine($"Сумма квадратов элементов матриц С и В*А расположенных после заданного элемента:{result}");
                             else
                                 Console.WriteLine($"Некоректный выбор элемента");
                         }
@@ -84,14 +85,12 @@ namespace UserInterface
                                 Matrix resultProduction = new Matrix();
                                 if (Matrix.ScalarMultiple(matrixC, matrixB, ref resultProduction))
                                 {
-                                    Console.WriteLine("Результат произведения С*В:");
-                                    resultProduction.ShowMatrix();
+                                    Console.WriteLine("Матрицы не равны,результат произведения С*В:");
+                                    ShowMatrix(resultProduction);
                                 }
                                 else
-                                    Console.WriteLine("Произведение C на B невозможно");
+                                    Console.WriteLine("Матрицы не равны,произведение C на B невозможно");
                             }
-                                
-                            Console.WriteLine($"Матрицы не равны,результат произведения");
                         }
                         else
                             Console.WriteLine("Нужно ввести матрицы");
@@ -102,10 +101,12 @@ namespace UserInterface
                 }
             }
         }
+
         /// <summary>
-        /// Prompts the user to make a choice
+        /// Prompts to user to make a choice
         /// </summary>
         /// <returns>User selection</returns>
+
         static int InputChoice()
         {
             int choice;
@@ -118,16 +119,79 @@ namespace UserInterface
             int.TryParse(input, out choice);
             return choice;
         }
+
+        /// <summary>
+        /// Inputes a matrix
+        /// </summary>
+        /// <returns>The value is true if the matrix input was completed correctly; otherwise, the value is false</returns>
+
+        public static bool InputMatrix(ref Matrix matrix)
+        {
+            int rows;
+            int columns;
+            bool resultInput = true;
+            Console.WriteLine("Введите количество строк матрицы ");
+            int.TryParse(Console.ReadLine(), out rows);
+            Console.WriteLine("Введите количество столбцов матрицы ");
+            int.TryParse(Console.ReadLine(), out columns);
+            if (rows > 0 && columns > 0)
+            {
+                double[,] tempMassiv = new double[rows, columns];
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        Console.WriteLine($"Введите элемент {i + 1}-строки {j + 1}-столбца ");
+                        if (!double.TryParse(Console.ReadLine(), out tempMassiv[i, j]))
+                        {
+                            Console.WriteLine("Некоректный ввод");
+                            resultInput = false;
+                        }
+                    }
+                }
+                if (resultInput)
+                {
+                    matrix.Numbers = tempMassiv;
+                    matrix.NumberRows = rows;
+                    matrix.NumberColumns = columns;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некоректный ввод количества строк-столбцов");
+                resultInput = false;
+            }
+            return resultInput;
+        }
+
+        /// <summary>
+        /// Outputs the matrix
+        /// </summary>
+
+        public static void ShowMatrix(Matrix matrix)
+        {
+            for (int i = 0; i < matrix.NumberRows; i++)
+            {
+                for (int j = 0; j < matrix.NumberColumns; j++)
+                {
+                    Console.Write($"{matrix.Numbers[i, j]} ");
+                }
+                Console.Write("\n");
+            }
+        }
+
         /// <summary>
         ///Calculates the sum of squares of matrix elements of large numbers 83.6
         /// </summary>
         /// <param name="A">The first matrix</param>
         /// <param name="B">The second matrix</param>
         /// <returns>Sum of sums of squares of two matrices</returns>
-        public static double CalculateNumberSumSquares(Matrix A,Matrix B)
+
+        public static double CalculateNumberSumSquares(Matrix A, Matrix B)
         {
             return A.CalculateSumSquares(83.6) + B.CalculateSumSquares(83.6);
         }
+
         /// <summary>
         /// Calculates the sum of squares of matrix elements located after the specified element
         /// </summary>
@@ -135,26 +199,28 @@ namespace UserInterface
         /// <param name="B">The second matrix</param>
         /// <param name="C">The third matrix</param>
         /// <returns>Sum of square sums</returns>
-        public static double CalculateLocatedSumSquares(Matrix A,Matrix B, Matrix C)
+
+        public static double CalculateLocatedSumSquares(Matrix A, Matrix B, Matrix C)
         {
             int row;
             int column;
             Matrix resultProduction = new Matrix();
-            Matrix.ScalarMultiple(B, A,ref resultProduction);
+            Matrix.ScalarMultiple(B, A, ref resultProduction);
             Console.WriteLine("Введите номер строки элемента");
             int.TryParse(Console.ReadLine(), out row);
             Console.WriteLine("Введите номер столбца элемента");
             int.TryParse(Console.ReadLine(), out column);
-            if(row == 0 || column == 0 || row > resultProduction.NumberRows || column > resultProduction.NumberColumns 
+            if (row == 0 || column == 0 || row > resultProduction.NumberRows || column > resultProduction.NumberColumns
                 || row > C.NumberRows || column > C.NumberColumns)
             {
                 return 0;
             }
             else
             {
-                return resultProduction.CalculateSumSquares(row, column)+C.CalculateSumSquares(row, column);
+                return resultProduction.CalculateSumSquares(row, column) + C.CalculateSumSquares(row, column);
             }
         }
+
         /// <summary>
         /// If the matrices are equal replaces the negative elements of the last column with zeros
         /// </summary>
@@ -162,7 +228,8 @@ namespace UserInterface
         /// <param name="B">The second matrix</param>
         /// <param name="C">The third matrix</param>
         /// <returns>True if the matrices are equal; otherwise, false</returns>
-        public static bool ReplaceNegativeNumbers(ref Matrix A,ref Matrix B,ref Matrix C)
+
+        public static bool ReplaceNegativeNumbers(ref Matrix A, ref Matrix B, ref Matrix C)
         {
             bool result = false;
             if (A == B && A == C)
@@ -178,7 +245,7 @@ namespace UserInterface
                 }
                 result = true;
             }
-            return result;        
+            return result;
         }
     }
 }
