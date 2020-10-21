b4c3a0d7568dea02be2f1ea3f930d259f1b21565
DenisDoroshko <de.doroshko@mail.ru>
2020-10-21 21:12:36 +0300

    third commit

---
+++ lab_work5/ParsingTests/PhoneParsingTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parsing;
+using System.Collections.Generic;
+
+namespace ParsingTests
+{
+    [TestClass]
+    public class PhoneParsingTest
+    {
+        [TestMethod]
+        public void ParsePhoneNumbers_TwoMobileNumbers()
+        {
+            //Arange
+            List<string> numbers = new List<string>() {"+375292303432","+7 911 234-12-12","11212121212"};
+            int expected = 2;
+            List<string> mobileNumbers = new List<string>();
+            List<string> homeNumbers = new List<string>();
+            List<string> incorrectNumbers = new List<string>();
+            //Act
+            PhoneParsing.ParsePhoneNumbers(numbers, mobileNumbers, homeNumbers, incorrectNumbers);
+            //Assert
+            Assert.AreEqual(expected, mobileNumbers.Count);
+        }
+
+        [TestMethod]
+        public void ParsePhoneNumbers_ThreeHomeNumbers()
+        {
+            //Arange
+            List<string> numbers = new List<string>() { "+375292303432", "23 5 31-34-12", "12 2 3 31-5-56","25 3 45-61-34" };
+            int expected = 3;
+            List<string> mobileNumbers = new List<string>();
+            List<string> homeNumbers = new List<string>();
+            List<string> incorrectNumbers = new List<string>();
+            //Act
+            PhoneParsing.ParsePhoneNumbers(numbers, mobileNumbers, homeNumbers, incorrectNumbers);
+            //Assert
+            Assert.AreEqual(expected, homeNumbers.Count);
+        }
+
+        [TestMethod]
+        public void ParsePhoneNumbers_ThreeIncorrectNumbers()
+        {
+            //Arange
+            List<string> numbers = new List<string>() { "+375292303432", "253 5 34-1h2", "12 2 3 31-5-56", "225 3 45-61-34","79112305489" };
+            int expected = 3;
+            List<string> mobileNumbers = new List<string>();
+            List<string> homeNumbers = new List<string>();
+            List<string> incorrectNumbers = new List<string>();
+            //Act
+            PhoneParsing.ParsePhoneNumbers(numbers, mobileNumbers, homeNumbers, incorrectNumbers);
+            //Assert
+            Assert.AreEqual(expected, incorrectNumbers.Count);
+        }
+        [TestMethod]
+        public void ParsePhoneNumbers_AllIncorrectNumbers()
+        {
+            //Arange
+            List<string> numbers = new List<string>() { "75292303432", "253 5 34-1h2", "12 2 3 31-5-56", "225 3 45-61-34", "1-1-1-1-1-1-1" };
+            int expected = 4;
+            List<string> mobileNumbers = new List<string>();
+            List<string> homeNumbers = new List<string>();
+            List<string> incorrectNumbers = new List<string>();
+            //Act
+            PhoneParsing.ParsePhoneNumbers(numbers, mobileNumbers, homeNumbers, incorrectNumbers);
+            //Assert
+            Assert.AreEqual(expected, incorrectNumbers.Count);
+        }
+    }
+}
---
+++ lab_work5/ParsingTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ParsingTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ParsingTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2324ed22-c751-4542-a020-3e709d2d0b5a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
